d3a94ad1f1f1f055907b49434de1c2d60c955851
caolixiang <caolixiang@gmail.com>
2026-02-28 00:00:47 +0800

    phase(6): switch websearch think icon to magnifier

--- src/components/markdown-content.tsx
+++ src/components/markdown-content.tsx
@@ -215,14 +215,14 @@ function WebSearchPrefixIcon() {
     <svg
       width="15"
       height="15"
-      viewBox="0 0 15 15"
+      viewBox="0 0 20 20"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       className="h-4 w-4 text-muted-foreground transition-all duration-200"
       aria-hidden="true"
     >
       <path
-        d="M6.1584 3.13508C6.35985 2.94621 6.67627 2.95642 6.86514 3.15788L10.6151 7.15788C10.7954 7.3502 10.7954 7.64949 10.6151 7.84182L6.86514 11.8418C6.67627 12.0433 6.35985 12.0535 6.1584 11.8646C5.95694 11.6757 5.94673 11.3593 6.1356 11.1579L9.565 7.49985L6.1356 3.84182C5.94673 3.64036 5.95694 3.32394 6.1584 3.13508Z"
+        d="M9 3a6 6 0 104.472 10.016l3.256 3.256a1 1 0 001.416-1.416l-3.256-3.256A6 6 0 009 3zm0 2a4 4 0 100 8 4 4 0 000-8z"
         fill="currentColor"
         fillRule="evenodd"
         clipRule="evenodd"
